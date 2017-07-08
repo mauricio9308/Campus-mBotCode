0a1bd9fccb682ba3e3542b7bf027dc3051566592
Mauricio Lara Soberanis <mauricio9308@gmail.com>
2017-07-08 09:44:27 -0500

    Final version of course

--- mBot/mBot.ino
+++ mBot/mBot.ino
@@ -44,20 +44,12 @@ int isPlayingMusic = 0;
 int receivedCommand;
 boolean newData = false;
 
-
 /*
 * Helper method for the handling of the 
 */
 void updateSpeed(int leftSpeed, int rightSpeed){
       motor_9.run((9)==M1?-(leftSpeed):(leftSpeed));
       motor_10.run((10)==M1?-(rightSpeed):(rightSpeed));
-}
-
-/**
-* Sample music playing of the Imperial march
-*/
-void starWars( ){
-    // TODO implement
 }
 
 void dance(){
@@ -78,6 +70,145 @@ void dance(){
     handle( MOVE_STOP, SLOW_SPEED );
     rgbled_7.setColor(1,255, 0, 255);
     rgbled_7.show();
+}
+
+/**
+* Sample music playing of the Imperial march
+*/
+void starWars(){
+    isPlayingMusic = 1;
+    buzzer.tone(a, 500);
+    buzzer.tone(a, 500);
+    buzzer.tone(a, 500);
+    buzzer.tone(f, 350);
+    buzzer.tone(cH, 150);
+    buzzer.tone(a, 500);
+    buzzer.tone(f, 350);
+    buzzer.tone(cH, 150);
+    buzzer.tone(a, 650);
+    
+    rgbled_7.setColor(1,0, 255, 213);
+    rgbled_7.show();
+    handle( MOVE_LEFT, SLOW_SPEED );
+    handle( MOVE_RIGHT, SLOW_SPEED );
+    handle( MOVE_STOP, SLOW_SPEED );
+ 
+    //end of first bit
+    
+    buzzer.tone(eH, 500);    
+    buzzer.tone(eH, 500);
+    buzzer.tone(eH, 500);
+    buzzer.tone(fH, 350);
+    buzzer.tone(cH, 150);
+    buzzer.tone(gS, 500);
+    buzzer.tone(f, 350);
+    buzzer.tone(cH, 150);
+    buzzer.tone(a, 650);
+    
+    rgbled_7.setColor(1,55, 98, 100);
+    rgbled_7.show();
+    handle( MOVE_FORWARD, SLOW_SPEED );
+    handle( MOVE_REVERSE, SLOW_SPEED );
+    handle( MOVE_STOP, SLOW_SPEED );
+ 
+    //end of second bit...
+ 
+    buzzer.tone(aH, 500);
+    buzzer.tone(a, 300);
+    buzzer.tone(a, 150);
+    buzzer.tone(aH, 400);
+    buzzer.tone(gSH, 200);
+    buzzer.tone(gH, 200);
+    buzzer.tone(fSH, 125);
+    buzzer.tone(fH, 125);
+    buzzer.tone(fSH, 250);
+
+    rgbled_7.setColor(1,0, 255,0);
+    rgbled_7.show();
+    handle( MOVE_REVERSE, SLOW_SPEED );
+    handle( MOVE_FORWARD, SLOW_SPEED );
+    handle( MOVE_STOP, SLOW_SPEED );
+ 
+    buzzer.tone(aS, 250);
+    buzzer.tone(dSH, 400);
+    buzzer.tone(dH, 200);
+    buzzer.tone(cSH, 200);
+    buzzer.tone(cH, 125);
+    buzzer.tone(b, 125);
+    buzzer.tone(cH, 250);
+
+    rgbled_7.setColor(1,0,0,255);
+    rgbled_7.show();
+    handle( MOVE_RIGHT, SLOW_SPEED );
+    handle( MOVE_LEFT, SLOW_SPEED );
+    handle( MOVE_STOP, SLOW_SPEED );
+ 
+    buzzer.tone(f, 125);
+    buzzer.tone(gS, 500);
+    buzzer.tone(f, 375);
+    buzzer.tone(a, 125);
+    buzzer.tone(cH, 500);
+    buzzer.tone(a, 375);
+    buzzer.tone(cH, 125);
+    buzzer.tone(eH, 650);
+
+    rgbled_7.setColor(1,255,0,0);
+    rgbled_7.show();
+    handle( MOVE_LEFT, SLOW_SPEED );
+    handle( MOVE_RIGHT, SLOW_SPEED );
+    handle( MOVE_STOP, SLOW_SPEED );
+    
+    //end of third bit... (Though it doesn't play well)
+    //let's repeat it
+ 
+    buzzer.tone(aH, 500);
+    buzzer.tone(a, 300);
+    buzzer.tone(a, 150);
+    buzzer.tone(aH, 400);
+    buzzer.tone(gSH, 200);
+    buzzer.tone(gH, 200);
+    buzzer.tone(fSH, 125);
+    buzzer.tone(fH, 125);
+    buzzer.tone(fSH, 250);
+
+    rgbled_7.setColor(1,0,255,0);
+    rgbled_7.show();
+    handle( MOVE_RIGHT, SLOW_SPEED );
+    handle( MOVE_LEFT, SLOW_SPEED );
+    handle( MOVE_STOP, SLOW_SPEED );
+
+    buzzer.tone(aS, 250);
+    buzzer.tone(dSH, 400);
+    buzzer.tone(dH, 200);
+    buzzer.tone(cSH, 200);
+    buzzer.tone(cH, 125);
+    buzzer.tone(b, 125);
+    buzzer.tone(cH, 250);
+
+    rgbled_7.setColor(1,0,0,255);
+    rgbled_7.show();
+    handle( MOVE_FORWARD, SLOW_SPEED );
+    handle( MOVE_REVERSE, SLOW_SPEED );
+    handle( MOVE_STOP, SLOW_SPEED );
+     
+    buzzer.tone(f, 250);
+    buzzer.tone(gS, 500);
+    buzzer.tone(f, 375);
+    buzzer.tone(cH, 125);
+    buzzer.tone(a, 500);
+    buzzer.tone(f, 375);
+    buzzer.tone(cH, 125);
+    buzzer.tone(a, 650);
+    //end of the song
+
+    rgbled_7.setColor(1,255,0,0);
+    rgbled_7.show();
+    handle( MOVE_LEFT, SLOW_SPEED );
+    handle( MOVE_RIGHT, SLOW_SPEED );
+    handle( MOVE_REVERSE, DEFAULT_SPEED );
+    handle( MOVE_FORWARD, DEFAULT_SPEED );
+    handle( MOVE_STOP, SLOW_SPEED );    
+    isPlayingMusic = 0;
 }
 
 void handle(int command, int speed){
@@ -117,6 +248,7 @@ void handle(int command, int speed){
         case MOVE_STOP:
           leftSpeed = 0;
           rightSpeed = 0;
+          break;
         default:
           leftSpeed = 0;
           rightSpeed = 0;
@@ -124,7 +256,6 @@ void handle(int command, int speed){
 
       updateSpeed(leftSpeed, rightSpeed);
 }
-
 
 void setup() {
     Serial.begin(115200);
@@ -145,10 +276,24 @@ void loop() {
     _delay(1.0);
 }
 void receiveCommand() {
-    // TODO implement
+    if (isPlayingMusic == 0 && Serial.available() > 0) {
+        receivedCommand = Serial.read();
+        newData = true;
+    }
 }
 void handleInput() {
-    // TODO implement
+    if (newData == true) {
+        Serial.print("This just in ... ");
+        Serial.println(receivedCommand, DEC);
+        newData = false;
+        rgbled_7.setColor(1,0,0,255);
+        rgbled_7.show();
+
+        handle( receivedCommand, DEFAULT_SPEED );
+    }else{
+        rgbled_7.setColor(1,255,0,0);
+        rgbled_7.show();
+    }
 }
 void _delay(float seconds){
     long endTime = millis() + seconds * 1000;
